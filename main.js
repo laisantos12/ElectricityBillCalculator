8b5d98bb4889e3807cc6b43ed20761437952e229
laisantos12 <72036199+laisantos12@users.noreply.github.com>
2022-05-17 17:02:01 +0100

    javascript update

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let calculateWindow;
 
 //Listen for app to be ready
 app.on('ready', function () {
@@ -16,4 +17,55 @@ app.on('ready', function () {
         protocol: 'file',
         slashes: true
     }));
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Isert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//Handle create calculate window
+function createCalculateWindow() {
+    //Create new window
+    calculateWindow = new BrowserWindow({
+        width: 200,
+        height: 300,
+        title: 'Please enter meter number'
+    });
+
+
+    //Load html into window
+    calculateWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'calculateWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+}
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'Menu',
+        submenu: [
+            {
+                label: 'Calculate Bill',
+                click() {
+                    creatCalculateWindow();
+                }
+
+            },
+            {
+                label: 'Clear'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+
+        ]
+    }
+];
