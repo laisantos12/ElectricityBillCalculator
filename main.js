3cd30f59eeb784cd64996f9a443d21b6dcd5e554
layhenececilio <layhenececilio@gmail.com>
2022-05-20 13:54:46 +0100

    changes

--- main.js
+++ main.js
@@ -7,6 +7,29 @@ const { app, BrowserWindow, Menu, ipcMain } = electron;
 let mainWindow;
 let calculateWindow;
 
+
+function calculateBill() {
+
+    var charges = 0.04;
+    var rate = 0.20;
+    var vat = 13.50;
+
+    var inputUnit = document.getElementById('number');
+    var inputDays = document.getElementById('period'); 
+
+    var billClean = inputUnit * rate + inputDays * charges;
+
+        if (inputUnit == 225 && inputDays == 60){
+
+         return  billClean + billClean * vat / 100;
+
+        }
+       
+        else{
+            return "Plese check your input and try again";
+        }
+    }
+
 //Listen for app to be ready
 app.on('ready', function () {
     //Create new window
